bf33fc0b671fa88f9d88ecef8a88f55d08eac8d8
matoi0317 <matoi.shibanuma@gmail.com>
2020-04-02 21:20:35 +0900

    self-study3

--- pages/index.vue
+++ pages/index.vue
@@ -14,17 +14,20 @@
     </div>
     <div class="rules">
       <ul>
+        <div class="a"><li><font color="red">・自習室のURLは09:50に配布します</font></li></div>
+        <div class="b"><li><font color="red">・自習室は火・木・金に開放しています</font></li></div>
         <li>・入室したらすぐに<font color="red">ミュート(消音)</font>にしてください</li>
         <li>・長時間退出する場合は<font color="red">挨拶をしてから</font>退出してください</li>
         <li>・なるべく手元を映してください</li>
         <li>・勉強の教え合い等話し合いがしたい場合は個別ルームを作るのでルーム長に声をかけてください</li>
         <li>・顔が移りそうな人は、プライバシーを守るために<font color="red">マスクをする</font>等対策をしてください</li>
         <li>・<font color="red">休憩時間のチャットは禁止です</font></li>
+        <li>・何歳でも参加可能なので、みんなで勉強しましょう！</li>
       </ul>
     </div>
     <div class="download">
       <h3>ZOOMのダウンロードはこちら</h3>
-      <a href="" class="button is-primary is-fullwifth">
+      <a href="https://apps.apple.com/jp/app/zoom-cloud-meetings/id546505307" class="button is-primary is-fullwifth">
         ZOOMをダウンロード
         </a>
     </div>
@@ -78,6 +81,14 @@ export default {}
 }
 .download h3 {
   margin-bottom: 10px;
+  font-weight: bold;
+}
+
+.a {
+  font-weight: bold;
+}
+
+.b {
   font-weight: bold;
 }
 </style>
