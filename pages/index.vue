55bd258534c3abe4fc1fcfc11ef86238b9638870
matoi0317 <matoi.shibanuma@gmail.com>
2020-04-15 09:59:14 +0900

    self9

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
       </div>
     </div>
     <div class="join">
-      <a href="" class="button is-primary is-fullwidth">
+      <a href="https://zoom.us/j/91521527247?pwd=a05qVlpKSEh0SVFKTVV0THVMUGFFQT09" class="button is-primary is-fullwidth">
         ガリ勉自習室に参加する
       </a>
       <div class="help-text">ZOOMが自動的に開きます</div>
